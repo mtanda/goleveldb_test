f96b50e864dcbff68ac2b5d74cac5a7ebd6d938e
Mitsuhiro Tanda <mitsuhiro.tanda@gree.net>
2015-03-17 17:14:30 +0900

    add flags

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"math/rand"
 
@@ -19,23 +20,32 @@ func randString() string {
 }
 
 func main() {
+	var verbose = flag.Bool("v", false, "verbose flag")
+	var nkey = flag.Int("k", 100, "number of keys")
+	var count = flag.Int("c", 10000, "put count")
+	flag.Parse()
+
 	rand.Seed(0)
-	keys := make([]string, 0, 100)
-	for i := 0; i < 100; i++ {
+	keys := make([]string, 0, *nkey)
+	for i := 0; i < *nkey; i++ {
 		keys = append(keys, randString())
 	}
 
 	db, _ := leveldb.OpenFile("./test_db", nil)
 	defer db.Close()
-	bs := make([]byte, 8)
-	n := 10000
-	for i := 0; i < n; i++ {
+	valueByte := make([]byte, 8)
+	for i := 0; i < *count; i++ {
 		for _, key := range keys {
 			num := rand.Int63()
-			binary.LittleEndian.PutUint64(bs, uint64(num))
-			db.Put([]byte(key), bs, nil)
-			//fmt.Printf("%s %d\n", key, uint64(num))
+			value := uint64(num)
+			binary.LittleEndian.PutUint64(valueByte, value)
+			db.Put([]byte(key), valueByte, nil)
+			if *verbose {
+				fmt.Printf("PUT %s %d\n", key, value)
+			}
 		}
-		fmt.Printf("%d / %d\n", i, n)
+		if *verbose {
+			fmt.Printf("%d / %d\n", i, *count)
+		}
 	}
 }
