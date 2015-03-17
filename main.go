8b670d7fe4bf0edf2b6b249e2c144b9c7e06477b
Mitsuhiro Tanda <mitsuhiro@macbook-air.local>
2015-03-17 09:08:25 +0900

    update

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
-import "github.com/syndtr/goleveldb/leveldb"
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+func randString() string {
+	num := rand.Uint32()
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, num)
+	hash := md5.Sum(bs)
+	return hex.EncodeToString(hash[:])
+}
 
 func main() {
-	db, _ := leveldb.OpenFile("./test_db", nil)
-	for i := 0; i < 10000; i++ {
-		db.Put([]byte("key"), []byte("value"), nil)
+	rand.Seed(0)
+	keys := make([]string, 0, 100)
+	for i := 0; i < 100; i++ {
+		keys = append(keys, randString())
 	}
+
+	db, _ := leveldb.OpenFile("./test_db", nil)
 	defer db.Close()
+	bs := make([]byte, 8)
+	n := 10000
+	for i := 0; i < n; i++ {
+		for _, key := range keys {
+			num := rand.Int63()
+			binary.LittleEndian.PutUint64(bs, uint64(num))
+			db.Put([]byte(key), bs, nil)
+			//fmt.Printf("%s %d\n", key, uint64(num))
+		}
+		fmt.Printf("%d / %d\n", i, n)
+	}
 }
